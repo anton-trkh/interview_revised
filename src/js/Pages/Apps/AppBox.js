0c40d57331de8ae7245640f3a05b9c89f1e99f3b
Anton <anton.trkh@gmail.com>
2016-05-19 15:53:32 -0400

    apps ok

--- src/js/Pages/Apps/AppBox.js
+++ src/js/Pages/Apps/AppBox.js
@@ -4,43 +4,76 @@
 // Date: Apr. 28, 2016
 
 import React from 'react';
+import Spacer from "../Shared/Spacer.js";
 
 const AppBox = React.createClass({
     render(){
         return(
-            <div className='Apps__box--pos'>
-                <div className='Apps__box__1--pos Apps__box--cDim Apps__box--outline'>
-                    <img src="./Assets/Apps/group-2.png" alt="interop" className='Apps__box__icon--center35x26' />
-                </div>
-                <div className='Apps__box__2--pos Apps__box--cDim Apps__box--simpleBorder'>
-                    <img src="./Assets/Apps/page-1.png" alt="voms" className='Apps__box__icon--center40x28' />
-                </div>
-                <div className='Apps__box__3--pos Apps__box--cDim Apps__box--simpleBorder'>
-                    <img src="./Assets/Apps/page-2.png" alt="voms" className='Apps__box__icon--center40x28' />
-                </div>
-				
-				
-                <div className='Apps__box__4--pos Apps__box--cDim Apps__box--simpleBorder'>
-                    <img src="./Assets/Apps/group-2.png" alt="interop" className='Apps__box__icon--center35x26' />
-                </div>
-                <div className='Apps__box__5--pos Apps__box--cDim Apps__box--simpleBorder'>
-                    <img src="./Assets/Apps/group-2.png" alt="interop" className='Apps__box__icon--center35x26' />
-                </div>
-                <div className='Apps__box__6--pos Apps__box--cDim Apps__box--simpleBorder'>
-                    <img src="./Assets/Apps/group-2.png" alt="interop" className='Apps__box__icon--center35x26' />
-                </div>
-				
-				
-                <div className='Apps__box__7--pos Apps__box--cDim Apps__box--simpleBorder'>
-                    <img src="./Assets/Apps/group-2.png" alt="interop" className='Apps__box__icon--center35x26' />
-                </div>
-                <div className='Apps__box__8--pos Apps__box--cDim Apps__box--simpleBorder'>
-                    <img src="./Assets/Apps/group-2.png" alt="interop" className='Apps__box__icon--center35x26' />
-                </div>
-                <div className='Apps__box__9--pos Apps__box--cDim Apps__box--simpleBorder'>
-                    <img src="./Assets/Apps/group-2.png" alt="interop" className='Apps__box__icon--center35x26' />
-                </div>
-            </div>
+			<div>
+            <div className='row'>
+				<div className='col-xs-2' />
+				<div className='col-xs-3'>
+					<div className='Apps__box--cDim Apps__box--outline'>
+						<img src="./Assets/Apps/group-2.png" alt="interop" className='Apps__box__icon--center35x26' />
+					</div>
+				</div>
+				<div className='col-xs-3'>
+					<div className=' Apps__box--cDim Apps__box--simpleBorder'>
+						<img src="./Assets/Apps/page-1.png" alt="voms" className='Apps__box__icon--center40x28' />
+					</div>
+				</div>
+				<div className='col-xs-3'>
+					<div className=' Apps__box--cDim Apps__box--simpleBorder'>
+						<img src="./Assets/Apps/page-2.png" alt="voms" className='Apps__box__icon--center40x28' />
+					</div>
+				</div>
+				<div className='col-xs-1' />
+			</div>
+			
+			<Spacer/>
+			
+			<div className='row'>
+				<div className='col-xs-2' />
+				<div className='col-xs-3'>
+					<div className=' Apps__box--cDim Apps__box--simpleBorder'>
+						<img src="./Assets/Apps/group-2.png" alt="interop" className='Apps__box__icon--center35x26' />
+					</div>
+				</div>
+				<div className='col-xs-3'>
+					<div className=' Apps__box--cDim Apps__box--simpleBorder'>
+						<img src="./Assets/Apps/group-2.png" alt="interop" className='Apps__box__icon--center35x26' />
+					</div>
+				</div>
+				<div className='col-xs-3'>
+					<div className=' Apps__box--cDim Apps__box--simpleBorder'>
+						<img src="./Assets/Apps/group-2.png" alt="interop" className='Apps__box__icon--center35x26' />
+					</div>
+				</div>
+				<div className='col-xs-1' />
+			</div>
+			
+			<Spacer/>
+			
+			<div className='row'>
+				<div className='col-xs-2' />
+				<div className='col-xs-3'>
+					<div className=' Apps__box--cDim Apps__box--simpleBorder'>
+						<img src="./Assets/Apps/group-2.png" alt="interop" className='Apps__box__icon--center35x26' />
+					</div>
+				</div>
+				<div className='col-xs-3'>
+					<div className=' Apps__box--cDim Apps__box--simpleBorder'>
+						<img src="./Assets/Apps/group-2.png" alt="interop" className='Apps__box__icon--center35x26' />
+					</div>
+				</div>
+				<div className='col-xs-3'>
+					<div className=' Apps__box--cDim Apps__box--simpleBorder'>
+						<img src="./Assets/Apps/group-2.png" alt="interop" className='Apps__box__icon--center35x26' />
+					</div>	
+				</div>
+				<div className='col-xs-1' />
+			</div>
+			</div>
         );
     }
 
